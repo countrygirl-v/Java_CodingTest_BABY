76f1ef60a83de444fdeb151a2a64607c1c95d89e
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-05-26 19:23:45 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int temp = 0;
+        
+        for(int i = 0; i < numbers.length - i; i++){
+            for(int j = 0; j < numbers.length - 1; j++){
+                if(numbers[j] > numbers[j + 1]){
+                    temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        answer = numbers[numbers.length - 1] * numbers[numbers.length -2];
+        
+        return answer;
+    }
+}
