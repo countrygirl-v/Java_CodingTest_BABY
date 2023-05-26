6016822e2870c4fd13e67e14beae8fe591231e2c
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-05-27 03:19:31 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,16 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int five = n / 100000; // 9
-        int four = (n - five * 100000) / 10000; // 3
-        int three = (n - five * 100000 - four * 10000) / 1000;
-        int two = (n - five * 100000 - four * 10000 - three * 1000) / 100;
-        int one = (n - five * 100000 - four * 10000 - three * 1000 - two * 100) / 10;
-        int zero = n - five * 100000 - four * 10000 - three * 1000 - two * 100 - one * 10;
-
-        int answer = five + four + three + two + one + zero;
-
-        if(n == 1000000) answer = 1;
-        
+        int answer = 0;
+        while(n > 0){
+            answer += n % 10;
+            n /= 10;
+        }
         return answer;
     }
 }
