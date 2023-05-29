69cd8e47ac10781831ccc7dd1be5d52c77503bff
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-05-30 04:32:49 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        for(int i = 1; i < t + 1; i++) n *= 2;
+        answer = n;
+        return answer;
+    }
+}
