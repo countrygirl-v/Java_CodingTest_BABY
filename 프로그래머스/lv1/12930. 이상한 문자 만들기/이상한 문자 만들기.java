52a5a80c649b77f24c6a680e489e6f572f86968a
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-06-17 16:37:20 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 15.13 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] stringArray = s.split(" ");
+
+        for (int i = 0; i < stringArray.length; i++) {
+            String word = "";
+            for (int j = 0; j < stringArray[i].length(); j++) {
+                if (j % 2 == 0) word += Character.toUpperCase(stringArray[i].charAt(j));
+                else word += Character.toLowerCase(stringArray[i].charAt(j));
+            }
+            if (i == stringArray.length - 1) answer += word;
+            else answer += word + " ";
+        }
+        int backspace = s.length() - answer.length();
+        return answer + " ".repeat(backspace);
+    }
+}
