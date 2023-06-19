7c6ae67be543241e0d9fab343ba31343868b7cd2
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-06-19 21:27:59 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 8.24 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -3,36 +3,31 @@ import java.util.Collections;
 
 class Solution {
     public int[] solution(int[] arr) {
-        
+        int[] answer = new int[arr.length - 1];
         if (arr.length == 1) {
-            int[] answer = new int[]{-1};
-            return answer;
-        }
+            int[] length1 = {-1};
+            answer = length1;
+        } else {
 
-        // 제일 작은 요소 빼서 리턴
-        int[] result = new int[arr.length - 1];
+            Integer[] integerArray = Arrays.stream(arr).boxed().toArray(Integer[]::new);
 
-        Object[] array = Arrays.stream(arr)
-                .sorted()
-                .boxed().toArray();
-
-        Arrays.sort(array, Collections.reverseOrder());
-
-        for (int i = 0; i < result.length; i++) {
-            result[i] = (int) array[i];
-        }
-        int[] result2 = new int[result.length];
-        int k = 0;
-        for (int i = 0; i < arr.length; i++) {
-            // {1, 12, 32, 6, 3, 234, 5623, 6, 2}
-            int num = arr[i];
-            for (int j = 0; j < result.length; j++) {
-                if( num == result[j]){
-                    result2[k] = result[j];
-                    k++;
+            Arrays.sort(integerArray, Collections.reverseOrder());
+            
+            int[] tempArray = new int[arr.length - 1];
+            for (int i = 0; i < answer.length; i++) {
+                tempArray[i] = (int) integerArray[i];
+            }
+            int k = 0;
+            for (int i = 0; i < arr.length; i++) {
+                int arrayNumber = arr[i];
+                for (int j = 0; j < tempArray.length; j++) {
+                    if (arrayNumber == tempArray[j]) {
+                        answer[k] = tempArray[j];
+                        k++;
+                    }
                 }
             }
         }
-        return result2;
+        return answer;
     }
 }
