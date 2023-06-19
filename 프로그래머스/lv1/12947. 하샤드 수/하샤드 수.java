2f1f3e41c91e3a48e1a7a524a22afbc1fdf485cd
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-06-19 12:13:28 +0900

    [level 1] Title: 하샤드 수, Time: 0.01 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum = 0;
+        int num = x;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        answer = (x % sum == 0);
+        return answer;
+    }
+}
