31c40dfe27be582a0fc9031e6bbc6be6c9421f34
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-09-22 18:39:31 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.02 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -3,21 +3,12 @@ class Solution {
         int answer = 0;
         boolean zeroCheck = false;
 
-        for (int i : common) {
-            if(i == 0) {
-                zeroCheck = true;
-                break;
-            }
-        }
-
         for (int i = 0; i < common.length; i++) {
-            if(zeroCheck) break;
-            if((common[1] / common[0] == common[2] / common[1]) && (common[1] % common[0] == 0)) {                answer = common[common.length-1] * (common[2] / common[1]);
+            if (common[1] - common[0] == common[2] - common[1]) {
+                answer = common[common.length - 1] + (common[2] - common[1]);
             }
-        }
-        for (int i = 0; i < common.length; i++) {
-            if(common[1] - common[0] == common[2] - common[1]) {
-                answer = common[common.length-1] + (common[2] - common[1]);
+            else if((common[1] / common[0] == common[2] / common[1]) && (common[1] % common[0] == 0)) {
+                answer = common[common.length-1] * (common[2] / common[1]);
             }
         }
         return answer;
