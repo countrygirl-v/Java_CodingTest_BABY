89d630e324fc120d1b194c94e3a929162b7c17c0
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-09-24 01:04:53 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -6,7 +6,7 @@ class Solution {
         List<String> s = new ArrayList<>();
         for (int i = 0; i < t.length() - (p.length() - 1); i++) {
             s.add(t.substring(i, i + p.length()));
-            if(Double.parseDouble(p) >= Double.parseDouble(s.get(i))) answer++;
+            if(Long.parseLong(p) >= Long.parseLong(s.get(i))) answer++;
         }
         return answer;
     }
