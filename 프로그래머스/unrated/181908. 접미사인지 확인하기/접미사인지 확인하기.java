497a4a42431f3864c12918068111aa00df4f11a5
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-10-12 15:46:19 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.10 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < my_string.length(); i++) {
+            list.add(my_string.substring(i));
+        }
+        return list.contains(is_suffix) ? 1 : 0;
+    }
+}
