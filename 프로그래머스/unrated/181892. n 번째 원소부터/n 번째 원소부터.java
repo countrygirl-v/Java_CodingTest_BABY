88f82ab0e845204838146db63ab8dc38b578007a
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-10-15 21:28:51 +0900

    [level unrated] Title: n 번째 원소부터, Time: 2.93 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,9 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = n - 1; i < num_list.length; i++) answer.add(num_list[i]);
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
