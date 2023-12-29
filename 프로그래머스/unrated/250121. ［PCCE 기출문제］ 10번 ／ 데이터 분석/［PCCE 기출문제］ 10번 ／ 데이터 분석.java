dbcd02b89b8872f93876b288ca88d3319833e902
CountryGirl <65941341+dpwls8364@users.noreply.github.com>
2023-12-29 09:59:40 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 9.00 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -1,52 +1,29 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 class Solution {
     public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
-        int[][] answer = new int[data.length][data[0].length];
-            List<int[]> answerList = new ArrayList<>();
-            int sortStandard = switch (sort_by) {
-                case "date" -> 1;
-                case "maximum" -> 2;
-                case "remain" -> 3;
-                default -> 0;
-            };
-            switch (ext) {
-                case "code" -> {
-                    for (int[] datum : data) {
-                        if (datum[0] < val_ext) {
-                            answerList.add(datum);
-                        }
-                    }
-                    answerList.sort(Comparator.comparingInt(a -> a[sortStandard]));
-                }
-                case "date" -> {
-                    for (int[] datum : data) {
-                        if (datum[1] < val_ext) {
-                            answerList.add(datum);
-                        }
-                    }
-                    answerList.sort(Comparator.comparingInt(a -> a[sortStandard]));
-                }
-                case "maximum" -> {
-                    for (int[] datum : data) {
-                        if (datum[2] < val_ext) {
-                            answerList.add(datum);
-                        }
-                    }
-                    answerList.sort(Comparator.comparingInt(a -> a[sortStandard]));
-                }
-                case "remain" -> {
-                    for (int[] datum : data) {
-                        if (datum[3] < val_ext) {
-                            answerList.add(datum);
-                        }
-                    }
-                    answerList.sort(Comparator.comparingInt(a -> a[sortStandard]));
-                }
-            }
-            return answerList.toArray(new int[0][]);
+        int sortStandard = switch (sort_by) {
+            case "date" -> 1;
+            case "maximum" -> 2;
+            case "remain" -> 3;
+            default -> 0;
+        };
+
+        int extIndex = switch (ext) {
+            case "date" -> 1;
+            case "maximum" -> 2;
+            case "remain" -> 3;
+            default -> 0;
+        };
+
+        List<int[]> answerList = Arrays.stream(data)
+                .filter(datum -> datum[extIndex] < val_ext)
+                .sorted(Comparator.comparingInt(a -> a[sortStandard]))
+                .collect(Collectors.toList());
+
+        return answerList.toArray(new int[0][]);
     }
 }
